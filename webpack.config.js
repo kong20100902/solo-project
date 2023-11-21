d75c7f8a06dfadd91d90fbf7d7d8e64cae7362e2
Wenzhen Gong <kong20100902@hotmail.com>
2023-11-20 19:33:54 -0800

    minor change

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
+    // publicPath: '/',
     filename: '[name].js',
   },
   mode: process.env.NODE_ENV,
